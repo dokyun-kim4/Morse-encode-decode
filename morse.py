a0da79b7bcff8c90c43de5ac02348c6dd744f405
Dokyun Kim <donny5557@gmail.com>
2023-06-27 06:05:19 -0400

    code for decoding and encoding

---
+++ morse.py
@@ -0,0 +1,78 @@
+"""
+Morse code encoder/decoder
+"""
+
+MORSE_TABLE = {
+    'A':".-",
+    'B':"-...",
+    'C':"-.-.",
+    'D':"-..",
+    "E":".",
+    "F":"..-.",
+    "G":"--.",
+    "H":"....",
+    "I":"..",
+    "J":".---",
+    "K":"-.-",
+    "L":".-..",
+    "M":"--",
+    "N":"-.",
+    "O":"---",
+    "P":".--.",
+    "Q":"--.-",
+    "R":".-.",
+    "S":"...",
+    "T":"-",
+    "U":"..-",
+    "V":"...-",
+    "W":".--",
+    "X":"-..-",
+    "Y":"-.--",
+    "Z":"--..",
+
+    "1":".----",
+    "2":"..---",
+    "3":"...--",
+    "4":"....-",
+    "5":".....",
+    "6":"-....",
+    "7":"--...",
+    "8":"---..",
+    "9":"----.",
+    "0":"-----",
+
+    " ":"/"
+    }
+
+
+
+def encode_morse(text)->str:
+    """
+    Given a string, returns the morse code equivalent
+
+    Args:
+        text: string to encode
+    
+    Returns:
+        morse: encoded string in morse code
+    """
+    chars = [char for char in text.upper()]
+    morse = " ".join([MORSE_TABLE[char] for char in chars])
+    return morse
+
+
+def decode_morse(code)->str:
+    """
+    Given morse code, returns the original string
+
+    Args:
+        code: morse code to decode
+
+    Returns:
+        decoded: decoded morse code
+    """
+    MORSE_INVERSE = dict((morse, letter) for letter, morse in MORSE_TABLE.items())
+    
+    morse_list = code.split(" ")
+    decoded = "".join([MORSE_INVERSE[code] for code in morse_list])
+    return decoded
